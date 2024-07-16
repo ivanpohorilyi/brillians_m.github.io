8d20ab6392239e333d616fadd3fb1c49635c5fc0
ivanpohorilyi <thepatrickmaps@gmail.com>
2024-07-16 18:05:24 +0300

    sixth commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+$(document).ready(function () {
+
+  $(".filter-button").click(function () {
+    var value = $(this).attr('data-filter');
+
+    if (value == "all") {
+      //$('.filter').removeClass('hidden');
+      $('.filter').show('1000');
+    }
+    else {
+      $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+      $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+      $(".filter").not('.' + value).hide('3000');
+      $('.filter').filter('.' + value).show('3000');
+
+    }
+  });
+
+  if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
+
+});
+
 var swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
@@ -10,7 +35,7 @@ var swiper = new Swiper(".swiper", {
     rotate: 0,
     stretch: 0,
     depth: 100,
-    modifier: 2,
+    modifier: 0,
     slideShadows: true
   },
   spaceBetween: 50,
